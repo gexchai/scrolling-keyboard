791a39fa585efb3e9e447fdf2c49c2ea9f75026d
Ellidi Jatgeirsson <ell@dhcp-39-99.tucknt.dartmouth.edu>
2012-11-16 10:10:22 -0500

    Initial Commit

---
+++ ScrollingKeyboard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollingKeyboard
+//
+//  Created by Ellidi Jatgeirsson on 11/16/12.
+//  Copyright (c) 2012 Ellidi Jatgeirsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
